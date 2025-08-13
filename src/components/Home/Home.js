790d96344e5a21522fae31a878b697577c3754e0
paradoxer-praveen <pk20050605@gmail.com>
2025-08-13 14:13:51 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Kalilrahaman</strong>
+                <strong className="main-name"> Dharani Aruna</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
